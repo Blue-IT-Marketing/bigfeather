4671dd39f08f6985f1dafc823dbb31f44bd581ff
justice <mobiusndou@gmail.com>
2019-06-29 18:00:37 +0200

    eslint update

---
+++ src/slack/index.js
@@ -0,0 +1,31 @@
+import Axios from "axios";
+
+
+
+export let sendMessageToSlack = async (channel,hook,message) => {
+    
+    let result = {status:false,error:{code:'',message:''}};
+    try{
+        let slackBody = {
+            mkdwn:true,
+            text: 'Status Message from : ' + app_name,
+            attachments: message.map(data => ({
+                color:'good',
+                text:`{...data}`
+            }))
+        }
+        await  Axios.post(channel+hook,).then( result => {
+            if (result.status === 200){
+                return result.data;
+            }
+        }).then(slack_message => {
+            result.status = true;
+            result.error.code = 200;
+            result.error.message = slack_message;
+        })
+    }catch(e){
+        console.log('Could not send message with error', e);
+    }
+
+    return result;
+}
