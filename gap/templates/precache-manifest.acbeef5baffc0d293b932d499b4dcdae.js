1b0af917bde2b96f4195f168c762ede75a070c0c
justice <mobiusndou@gmail.com>
2019-07-22 19:11:14 +0200

    added google app engine backend
    added google app engine backend support

---
+++ gap/templates/precache-manifest.acbeef5baffc0d293b932d499b4dcdae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a13e8b41a94b0b1ca0f4bf5ba11b317",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbf05a35d950076b8671",
+    "url": "/static/css/main.7a3d772b.chunk.css"
+  },
+  {
+    "revision": "64ee33366b290d9fe2cf",
+    "url": "/static/js/2.bda1d800.chunk.js"
+  },
+  {
+    "revision": "bbf05a35d950076b8671",
+    "url": "/static/js/main.16f52ae3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
