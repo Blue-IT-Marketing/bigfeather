4294ee99886fff1e38c90a5cbf1a2b30255fd5ae
justice <mobiusndou@gmail.com>
2019-07-24 20:59:11 +0200

    Update auth-actions.js

--- src/actions/auth-actions.js
+++ src/actions/auth-actions.js
@@ -3,40 +3,222 @@ import {
 } from '../firebase';
 import {
     account_details_type
-} from '../../types';
+} from '../types';
 
-export const AUTH_ACTIONS = {
-    DO_LOGIN : 'USER:DO_LOGIN',
-    DO_LOGOUT: 'USER:DO_LOGOUT',
-    DO_CHANGE_PASSWORD: 'USER:DO_CHANGE_PASSWORD',
-    DO_SUBSCRIBE: 'USER:DO_SUBSCRIBE',
-    DO_SEND_RECOVERY_EMAIL: 'USER:DO_SEND_RECOVERY_EMAIL'
+export const USER_ACCOUNT_ACTIONS = {
+    USER_LOGGED_IN: 'USER:USER_LOGGED_IN',
+    USER_SIGNED_OUT: 'USER:USER_SIGNED_OUT',
+    CREATE_USER: 'USER:CREATE_USER',
+    LOGIN_USER: 'USER:LOGIN_USER',
+    LOGOUT_USER: 'USER:LOGOUT_USER',
+    DELETE_USER: 'USER:DELETE_USER',
+    CHANGE_PASSWORD: 'USER:CHANGE_PASSWORD',
+    SEND_VERIFICATION_EMAIL: 'USER:SEND_VERIFICATION_EMAIL',
+    SEND_ONETIME_PIN: 'USER:SEND_ONETIME_PIN'
 };
 
-export const doDologin_Action = (username,password) => {
-    console.log('do login action ', username, ':',password);
+export const doDispatchUserSignedIn = (user) => {
+    let user_account = {
+        ...account_details_type
+    };
+    user_account = {
+        ...user
+    };
+    user_account.user_signed_in = true;
     return dispatch => {
-		auth.doSignInWithEmailAndPassword(username, password).then(result => {
-		    dispatch(result.status ? {
-		        type: AUTH_ACTIONS.DO_LOGIN,
-		        payload: {
-		            user_account: {...result.response},
-		            message: 'user logged in',
-		            code: 200
-		        }
-		    } : {
-                type: AUTH_ACTIONS.DO_LOGIN,
-		        payload: {
-		            user_account: {
-		                ...account_details_type
-		            },
-		            message: result.error.message,
-		            code: result.error.code
-		        }
-		    });
-		});        
-    }
-}
+        dispatch({
+            type: USER_ACCOUNT_ACTIONS.USER_LOGGED_IN,
+            user_account: {
+                ...user_account
+            },
+        });
+    };
+};
+
+export const doDispatchUserSignedOut = (user) => {
+    return dispatch => {
+        dispatch({
+            type: USER_ACCOUNT_ACTIONS.USER_SIGNED_OUT,
+            user_account: {
+                ...user
+            }
+        });
+    };
+};
+
+export const doDispatchCreateUser = (username, password) => {
+    return dispatch => {
+        auth.doCreateUserWithEmailAndPassword(username, password).then(result => {
+            dispatch(result.status ? {
+                type: USER_ACCOUNT_ACTIONS.CREATE_USER,
+                payload: {
+                    user_account: {
+                        ...result.response
+                    },
+                    message: 'user logged in',
+                    code: 200
+
+                }
+            } : {
+                type: USER_ACCOUNT_ACTIONS.CREATE_USER,
+                payload: {
+                    user_account: {
+                        ...account_details_type
+                    },
+                    message: result.error.message,
+                    code: result.error.code
+                }
+            });
+        });
+    };
+};
 
 
+export const doDispatchLoginUser = (username, password) => {
+    return dispatch => {
+        auth.doSignInWithEmailAndPassword(username, password).then(result => {
+            dispatch(result.status ? {
+                type: USER_ACCOUNT_ACTIONS.LOGIN_USER,
+                payload: {
+                    user_account: {
+                        ...result.response
+                    },
+                    message: 'user logged in',
+                    code: 200
 
+                }
+            } : {
+                type: USER_ACCOUNT_ACTIONS.LOGIN_USER,
+                payload: {
+                    user_account: {
+                        ...account_details_type
+                    },
+                    message: result.error.message,
+                    code: result.error.code
+                }
+            });
+        });
+    };
+};
+
+export const doDispatchLogOutUser = (user) => {
+    return dispatch => {
+        auth.doSignOut(user).then(result => {
+            dispatch((result.status === true) ? {
+                type: USER_ACCOUNT_ACTIONS.LOGOUT_USER,
+                payload: {
+                    user_account: {
+                        ...account_details_type
+                    },
+                    message: 'user logged out',
+                    code: 200
+                },
+            } : {
+                type: USER_ACCOUNT_ACTIONS.LOGOUT_USER,
+                payload: {
+                    user_account: {
+                        ...user
+                    },
+                    message: result.error.message,
+                    code: result.error.code
+                },
+            });
+        });
+    };
+};
+
+export const doDispatchDeleteUser = (user) => {
+    //User should be a record of currentUser
+    return dispatch => {
+        auth.doDeleteUser(user).then(result => {
+            dispatch(result.status === true ? {
+                type: USER_ACCOUNT_ACTIONS.DELETE_USER,
+                payload: {
+                    user_account: {
+                        ...account_details_type
+                    },
+                    message: 'user successfully deleted',
+                    code: 200
+                }
+            } : {
+                type: USER_ACCOUNT_ACTIONS.DELETE_USER,
+                payload: {
+                    user_account: {
+                        ...user
+                    },
+                    message: result.error.message,
+                    code: result.error.code
+                }
+            });
+        });
+    };
+};
+
+export const doDispatchChangePassword = (user) => {
+    return dispatch => {
+        auth.doPasswordUpdate(user.password).then(result => {
+            dispatch((result.status === true) ? {
+                type: USER_ACCOUNT_ACTIONS.CHANGE_PASSWORD,
+                payload: {
+                    user_account: {
+                        ...user
+                    },
+                    message: 'password successfully updated',
+                    code: 200
+                },
+            } : {
+                type: USER_ACCOUNT_ACTIONS.CHANGE_PASSWORD,
+                payload: {
+                    message: result.error.message,
+                    code: result.error.code
+                }
+            });
+        });
+    };
+};
+
+export const doSendVerificationEmail = (user) => {
+    return dispatch => {
+        auth.doSendEmailVerification(user).then(result => {
+            dispatch((result.status === true) ? {
+                type: USER_ACCOUNT_ACTIONS.SEND_VERIFICATION_EMAIL,
+                payload: {
+                    user_account: {
+                        ...result.response
+                    },
+                    message: 'email verification successfully sent',
+                    code: 200
+                },
+            } : {
+                type: USER_ACCOUNT_ACTIONS.SEND_VERIFICATION_EMAIL,
+                payload: {
+                    message: result.error.message,
+                    code: result.error.code
+                }
+            });
+        });
+    };
+};
+
+export const doSendOneTimePin = (user) => {
+    return dispatch => {
+        auth.doSendOneTimePin(user).then(result => {
+            dispatch((result.status === true) ? {
+                type: USER_ACCOUNT_ACTIONS.SEND_ONETIME_PIN,
+                payload: {
+                    user_account: {
+                        ...result.response
+                    },
+                    message: 'One Time Pin successfully sent',
+                    code: 200
+                },
+            } : {
+                type: USER_ACCOUNT_ACTIONS.SEND_ONETIME_PIN,
+                payload: {
+                    message: result.error.message,
+                    code: result.error.code
+                }
+            });
+        });
+    };
+};
