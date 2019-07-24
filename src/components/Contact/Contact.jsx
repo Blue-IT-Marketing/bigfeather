d694f0a2e6536855e6741262a5f7ae61caccdb81
justice <mobiusndou@gmail.com>
2019-07-24 20:15:15 +0200

    init

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,98 +1,112 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 
-export default function Contact() {
+export default class Contact extends Component {
+    constructor(){
+        super();
+        this.state = {
+            contant : {}
+        }
+    };
 
-
-    let ResetDefault = e => {
+     ResetDefault (e) {
         if (e.target.value === 'Message...') {
             e.target.value = ''
         }
     }
 
-    let SubmitHandler = e => {
+    SubmitHandler (e){
         e.preventDefault();
         console.log('Submitting Contact Form')
     }
-    return (
-        <Fragment>            
-            <div className='box box-body'>
+    
 
-                <div className='box-header'>
-                    <h3 className='box-title'> <strong> <i className='fa fa-envelope'> </i> Contact</strong></h3>
-                </div>
+    render() {
+      return (
+          <Fragment>
+              <div className='box box-body'>
+
+                  <div className='box-header'>
+                      <h3 className='box-title'> <strong> <i className='fa fa-envelope'> </i> Contact</strong></h3>
+                  </div>
 
 
-                <div className='box-footer'>
-                    <form className='form-horizontal' onSubmit={e => SubmitHandler(e)}>
-                        <div className='form-group'>
+                  <div className='box-footer'>
+                      <form className='form-horizontal' onSubmit={e => SubmitHandler(e)}>
+                          <div className='form-group'>
 
-                            <input
-                                type='text'
-                                className='form-control' 
-                                name='names' 
-                                placeholder='Names' 
-                            />
-                        </div>
+                              <input
+                                  type='text'
+                                  className='form-control'
+                                  name='names'
+                                  placeholder='Names'
+                              />
+                          </div>
 
-                        <div className='form-group'>
+                          <div className='form-group'>
 
-                            <input 
-                                type='text' 
-                                className='form-control' 
-                                name='surname'
-                                placeholder='Surname' 
-                            />
-                        </div>
+                              <input
+                                  type='text'
+                                  className='form-control'
+                                  name='surname'
+                                  placeholder='Surname'
+                              />
+                          </div>
 
-                        <div className='form-group'>
-                            <input
-                                type='text'
-                                className='form-control'
-                                name='cell'
-                                placeholder='Cell'
+                          <div className='form-group'>
+                              <input
+                                  type='text'
+                                  className='form-control'
+                                  name='cell'
+                                  placeholder='Cell'
 
-                            />
-                        </div>
+                              />
+                          </div>
 
-                        <div className='form-group'>
-                            <input 
-                                type='email'
-                                className='form-control'
-                                name='email'
-                                placeholder='Email'
-                            />                        
-                        </div>
+                          <div className='form-group'>
+                              <input
+                                  type='email'
+                                  className='form-control'
+                                  name='email'
+                                  placeholder='Email'
+                              />
+                          </div>
 
-                        <div className='form-group'>
-                            <input
-                                type='text'
-                                className='form-control'
-                                name='subject'
-                                placeholder='Subject'
-                            />
-                        </div>
+                          <div className='form-group'>
+                              <input
+                                  type='text'
+                                  className='form-control'
+                                  name='subject'
+                                  placeholder='Subject'
+                              />
+                          </div>
 
-                        <div className='form-group'>
+                          <div className='form-group'>
 
-                            <textarea
-                                name='message'
-                                placeholder='Message...'
-                                className='form-control'
-                            ></textarea>
-                        </div>
+                              <textarea
+                                  name='message'
+                                  placeholder='Message...'
+                                  className='form-control'
+                              ></textarea>
+                          </div>
 
-                        <div className='form-group'>
-                            <button className='btn btn-success btn-block btn-lg'><strong> <i className='fa fa-send'> </i> Send Message</strong></button>
-                        </div>
+                          <div className='form-group'>
+                              <button className='btn btn-success btn-block btn-lg'><strong> <i className='fa fa-send'> </i> Send Message</strong></button>
+                          </div>
 
-                    </form>
+                      </form>
 
-                </div>
+                  </div>
 
-            </div>
+              </div>
 
-        </Fragment>
+          </Fragment>
 
-    )
+      )
+  }
 }
+
+
+
+
+
